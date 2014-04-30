0bd8507a2ff2bdbc505e872a33f806be57722a14
dorian <dorian@lunarpixels.com>
2014-04-30 22:50:33 +0300

    xx

--- bin/nemesis.php
+++ bin/nemesis.php
@@ -10,6 +10,7 @@
     $pull->bind('tcp://127.0.0.1:5555'); // Binding to 127.0.0.1 means the only client that can connect is itself
     $pull->on('message', array($pusher, 'onNudgePost'));
 
+    echo 'Pull Done';
     // Set up our WebSocket server for clients wanting real-time updates
     $webSock = new React\Socket\Server($loop);
     $webSock->listen(8088, '0.0.0.0'); // Binding to 0.0.0.0 means remotes can connect
