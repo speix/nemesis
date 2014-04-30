a0b7100f94fe635b1fb7112cd28fcbf5c57e6859
dorian <dorian@lunarpixels.com>
2014-04-30 16:19:49 +0300

    Getting there

--- bin/nemesis.php
+++ bin/nemesis.php
@@ -8,7 +8,7 @@
     $context = new React\ZMQ\Context($loop);
     $pull = $context->getSocket(ZMQ::SOCKET_PULL);
     $pull->bind('tcp://127.0.0.1:5555'); // Binding to 127.0.0.1 means the only client that can connect is itself
-    $pull->on('message', array($pusher, 'onNudgeEntry'));
+    $pull->on('message', array($pusher, 'onNudgePost'));
 
     // Set up our WebSocket server for clients wanting real-time updates
     $webSock = new React\Socket\Server($loop);
