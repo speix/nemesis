4276309b59111047747a90b1e7cb7d63209211fd
dorian <dorian@lunarpixels.com>
2014-05-01 11:35:27 +0300

    .

--- src/Nemesis/Pusher.php
+++ src/Nemesis/Pusher.php
@@ -47,12 +47,12 @@ class Pusher implements WampServerInterface {
     
     public function onCall(ConnectionInterface $conn, $id, $topic, array $params) {
         // In this application if clients send data it's because the user hacked around in console
-       // $conn->callError($id, $topic, 'You are not allowed to make calls')->close();
+        $conn->callError($id, $topic, 'You are not allowed to make calls')->close();
     }
     
     public function onPublish(ConnectionInterface $conn, $topic, $event, array $exclude, array $eligible) {
         // In this application if clients send data it's because the user hacked around in console
-        //$conn->close();
+        $conn->close();
     }
     
     public function onError(ConnectionInterface $conn, \Exception $e) {
